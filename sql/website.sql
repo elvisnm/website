5d84af6b0b6e613a12f34d276c6630996d53c9bc
Elvis Moreira <elvisnm@gmail.com>
2011-08-05 13:55:11 -0300

    Add script sql.

---
+++ sql/website.sql
@@ -0,0 +1,58 @@
+-- MySQL Administrator dump 1.4
+--
+-- ------------------------------------------------------
+-- Server version	5.1.44
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+
+
+--
+-- Create schema website
+--
+
+CREATE DATABASE IF NOT EXISTS website;
+USE website;
+
+--
+-- Definition of table `website`.`website_pages`
+--
+
+DROP TABLE IF EXISTS `website`.`website_pages`;
+CREATE TABLE  `website`.`website_pages` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `url` varchar(160) NOT NULL DEFAULT '',
+  `template` varchar(160) NOT NULL DEFAULT '',
+  `action` varchar(160) NOT NULL DEFAULT '',
+  `h1` varchar(120) NOT NULL DEFAULT '',
+  `description` varchar(250) NOT NULL DEFAULT '',
+  `keywords` varchar(300) NOT NULL DEFAULT '',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `website`.`website_pages`
+--
+
+/*!40000 ALTER TABLE `website_pages` DISABLE KEYS */;
+LOCK TABLES `website_pages` WRITE;
+UNLOCK TABLES;
+/*!40000 ALTER TABLE `website_pages` ENABLE KEYS */;
+
+
+
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
